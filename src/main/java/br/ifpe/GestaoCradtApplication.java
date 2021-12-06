4fa5b490a2ea0cd8c4cb652adb1fb1f7a2306069
Francisco do Nascimento Junior <chico.nascimento@gmail.com>
2021-12-06 00:41:20 -0300

    Commit criação de projeto

---
+++ src/main/java/br/ifpe/GestaoCradtApplication.java
@@ -0,0 +1,13 @@
+package br.ifpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCradtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCradtApplication.class, args);
+	}
+
+}
